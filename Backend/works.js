ba1e900269c2e197ad7b35f5a104b0f1729caa93
Gasmi Yassine <gasmi.yassine92@gmail.com>
2023-11-08 17:18:03 +0100

    Integration of filters

--- Backend/works.js
+++ Backend/works.js
@@ -1,49 +1,77 @@
 // const { categories } = require("./models");
+const gallery = document.querySelector(".gallery");
+const portfolio = document.querySelector("#portfolio");
+const filterBtn = document.querySelector(".filters");
+const ul = document.querySelector(".filters ul");
+const allWorks = document.querySelector(".gallery");
+let selectedFiterId;
+let categoriesName = [];
+let categoriesId = [];
 
-async function getData() {
+
+async function getData(filter) {
     const response = await fetch("http://localhost:5678/api/works");
     const worksElements = await response.json();
     // waits until the request completes...
-    let data = worksElements;
-    displayWorks(data, 0);
-  }
-
-function displayWorks(works, filter)
-{
-    const gallery = document.querySelector(".gallery");
-    console.log(works);
-            let categories = [];
-
-    if (works.length>0)
-    {
-        for (let i = 0; i < works.length; i++){
-        let workContainer = document.createElement("figure");
-        let imgWork = document.createElement("img");
-        let imgTitle = document.createElement("figcaption");
-        imgWork.src = works[i].imageUrl;
-        imgWork.alt = works[i].title;
-        if(categories.indexOf(works[i].category.name) === -1){
-            categories.push(works[i].category.name)
+    if(worksElements){
+        if(filter !== 0){
+            console.log("hello" + filter);
+            const filtered = worksElements.filter(function(i) {
+                return i.categoryId === filter;
+              }); 
+              console.log(filtered);
+             displayWorks(filtered);
         }
-        imgTitle.innerHTML = works[i].title;
-        workContainer.appendChild(imgWork);
-        workContainer.appendChild(imgTitle);
-        gallery.appendChild(workContainer);
+        else {
+            displayWorks(worksElements);
         }
-        generateFiltersBtn(categories);
-        console.log(categories);
-
     }
     else{
-        let portfolio = document.querySelector(".portfolio");
         portfolio.classList.add("hide-portolio");
+    }
+     
+  }
+
+function displayWorks(works)
+{
+     
+    console.log(works);
+    
+    if(portfolio.classList.contains("hide-portolio")){portfolio.classList.remove("hide-portolio");}
+
+
+        for (let i = 0; i < works.length; i++){
+        
+        let workContainer = document.createElement("figure");
+        let imgWork = document.createElement("img");
+        let imgCaption = document.createElement("figcaption");
+        imgWork.src = works[i].imageUrl;
+        imgWork.alt = works[i].title;
+        if(categoriesName.indexOf(works[i].category.name) === -1){
+            categoriesName.push(works[i].category.name)
+            categoriesId.push(works[i].category.id)
+        }
+        imgCaption.innerHTML = works[i].title;
+        workContainer.appendChild(imgWork);
+        workContainer.appendChild(imgCaption);
+        gallery.appendChild(workContainer);
+        }
+        if(ul.childElementCount === 0){generateFiltersBtn(categoriesName, categoriesId);}
+}
+
+function removeWorks()
+{
+    while(allWorks.lastElementChild)
+    {
+        allWorks.removeChild(allWorks.lastElementChild);
     }
 }
 
-function generateFiltersBtn(filtersName)
+
+
+
+function generateFiltersBtn(filtersName, filtersId)
 {
-    let filterBtn = document.querySelector(".filters")
-    let ul = document.createElement("ul")
 
     //We should verify that there is more than 1 category to add the filter "All"
     
@@ -51,6 +79,16 @@ function generateFiltersBtn(filtersName)
     {
         let li = document.createElement("li")
         li.innerHTML = "Tous";
+        li.id ="0";
+        selectedFiterId = li;
+        selectedFiterId.classList.add("selected");
+        selectedFiterId.classList.add("f0");
+        // li.classList.add("child");
+        // const li = `<li class ="child">Tous</li>`
+        // ul.insertAdjacentHTML("afterBegin", li);
+        // item[i].addEventListener("click", itemDone);
+    
+
         ul.appendChild(li);
     }
 
@@ -58,31 +96,48 @@ function generateFiltersBtn(filtersName)
     {
         let li = document.createElement("li")
         li.innerHTML = filtersName[i];
+        li.classList.add("f0");
+        li.id = "" + filtersId[i];
+        li.classList.add("f"+ (i+1));
+        // li.classList.add("child");
+        // const li = `<li class ="child">Tous</li>`
+        // ul.insertAdjacentHTML("afterBegin", li);
         ul.appendChild(li);
     }
     filterBtn.appendChild(ul);
 }
+
+getData(0);
+
+
 let selectedFilterElement;
 
-function initListener() {
-    const buttons = document.querySelectorAll(".filters ul li");
-    buttons.forEach((button) => button.addEventListener('click', () => {
-      console.log(button);
-      if (selectedFilterElement) {
-        selectedFilterElement.classList.remove('selected');
-      }
-      selectedFilterElement = button;
-      selectedFilterElement.classList.add('selected');
-  
-      displayWorks
-      // filterSelection(button.dataset.filter);
-    }));
+ ul.addEventListener("click", function(e) {
+    let target = e.target; // where was the click?
+    let target2 = e.target.id; // where was the click?
+    console.log("eeee" + target2);
+    //console.log(target.classList.contains("selected"));
+    //portfolio.innerHTML = "";
+    selectedFiterUpdate(target, target2);
+   });
+
+
+   
+let i = 2;
+   function selectedFiterUpdate(filter, filterId) {
+    if (selectedFiterId) { // remove the existing highlight if any
+        selectedFiterId.classList.remove('selected');
+        if(selectedFiterId){
+            removeWorks();getData(parseInt(filterId));}
+        else { }
+    }
+    selectedFiterId = filter;
+    selectedFiterId.classList.add('selected'); // highlight the new td
   }
- 
+  
 
 
-getData();
 
-initListener();
+
 
 
